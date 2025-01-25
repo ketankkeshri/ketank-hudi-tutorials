aa66fe27c881de0c81c42d2a7e236b29b2e5531c
Ketan Keshri <Ketan@MBK-ORIX-131.local>
2025-01-26 01:52:19 +0530

    hudi table create

---
+++ jars/download_url.sh
@@ -0,0 +1,3 @@
+wget https://repo1.maven.org/maven2/org/apache/spark/spark-avro_2.12/3.5.0/spark-avro_2.12-3.5.0.jar -P ./jars/
+
+wget https://repo1.maven.org/maven2/org/apache/hudi/hudi-spark3.5-bundle_2.12/1.0.0/hudi-spark3.5-bundle_2.12-1.0.0.jar -P ./jars/
